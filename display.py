9fadc42650b7647570147c4a921fa83f0534402d
Vadim Salavatov <vmsalavatov@gmail.com>
2021-03-30 21:38:03 +0300

    display.py: -train acc, +param deviation

--- display.py
+++ display.py
@@ -9,64 +9,77 @@ from model_statistics import ModelStatistics
 class Finish:
     def __init__(self):
         self.__finished = False
-        
+
     def __call__(self, signum, stack):
         if self.__finished:
             exit()
         self.__finished = True
-    
+
     def finished(self):
         return self.__finished
-    
 
-def plot_loop(names, paths, title, save=None):
+
+def plot_loop(names, paths, title, save=None, param_dev=None):
     finish = Finish()
     signal.signal(signal.SIGINT, finish)
-    
+
     plt.rcParams['font.size'] = 18
     plt.rcParams['axes.facecolor'] = 'white'
     plt.rcParams['figure.facecolor'] = 'white'
-        
-    while not finish.finished():    
+
+    while not finish.finished():
         stats = {name: ModelStatistics.load_from_file(path) for name, path in zip(names, paths)}
-        
-        fig, (plt_loss, plt_train_acc, plt_val_acc) = plt.subplots(3, 1)
-        fig.set_size_inches(18, 20)
+        param_dev_stats = ModelStatistics.load_from_file(param_dev) if param_dev else None
+
+        if param_dev_stats:
+            fig, (plt_loss, plt_param_dev, plt_val_acc) = plt.subplots(3, 1)
+            fig.set_size_inches(18, 20)
+        else:
+            plt_param_dev = None
+            fig, (plt_loss, plt_val_acc) = plt.subplots(2, 1)
+            fig.set_size_inches(18, 20 * 0.7)
         fig.suptitle(title, fontsize=20)
         fig.tight_layout()
-        
+
         plt_loss.set_ylabel('Loss (local)')
         plt_loss.set_yscale('log')
         plt_loss.set_xlabel('Epoch')
-        
-        plt_train_acc.set_ylabel('Train Accuracy, %')
-        plt_train_acc.set_xlabel('Epoch')
+
+        if param_dev_stats:
+            plt_param_dev.set_ylabel('Parameter deviation (coef. of variation)')
+            plt_param_dev.set_xlabel('Epoch')
+            plt_param_dev.set_yscale('log')
 
         plt_val_acc.set_ylabel('Validation Accuracy, %')
         plt_val_acc.set_xlabel('Epoch')
-    
+
         for label, stat in stats.items():
             loss = stat.crop('train_loss')
-            train_acc = stat.crop('train_precision')
             val_acc = stat.crop('val_precision')
-                
+
             plt_loss.plot(range(len(loss)), loss, label=label)
-            plt_train_acc.plot(range(len(train_acc)), train_acc, label=label)
             plt_val_acc.plot(range(len(val_acc)), val_acc, label=label)
-    
+
+        if param_dev_stats:
+            batches_per_epoch = next(iter(param_dev_stats.crop('batches_per_epoch')[0].values()))
+            deviation = param_dev_stats.crop('coef_of_var')
+            plt_param_dev.plot([b / batches_per_epoch for b in range(len(deviation))],
+                                deviation, label='max coef. of variation')
+
         plt_loss.legend()
         plt_val_acc.legend()
-        plt_train_acc.legend()
+        if param_dev_stats:
+            plt_param_dev.legend()
 
         plt.close(fig)
-        clear_output(wait = True)
+        clear_output(wait=True)
         display(fig)
-    
+
         if save is not None:
             fig.savefig(save)
 
         time.sleep(5.0)
-        
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Online model statistics display')
@@ -74,6 +87,8 @@ if __name__ == '__main__':
     parser.add_argument('--paths', '-p', nargs='+')
     parser.add_argument('--title', '-t', required=True)
     parser.add_argument('--save', '-s', nargs='?', default=None)
+    parser.add_argument('--param-dev', required=False, default=None,
+                        help='model statistics file with parameter deviation (produced by master)')
     args = parser.parse_args()
-    
-    plot_loop(args.names, args.paths, args.title, args.save)
+
+    plot_loop(args.names, args.paths, args.title, args.save, args.param_dev)
